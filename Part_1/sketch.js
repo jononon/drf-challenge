a5d2a04d2edfa1507831e67c8481355bf88c88dc
Jonathan Damico <jj_damico@yahoo.com>
2019-09-28 17:58:19 +0100

    push to secure site

--- Part_1/sketch.js
+++ Part_1/sketch.js
@@ -1,11 +1,18 @@
 var mic;
 
 function setup () {
+	noLoop();
+}
+
+function audioStart() {
 	mic = new p5.AudioIn();
 	mic.start();
+	loop();
 }
 
 function draw () {
 	var vol = mic.getLevel();
 	console.log(vol);
 }
+
+function mousePressed() { getAudioContext().resume() }
